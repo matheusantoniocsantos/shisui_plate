49d9a15a4c7dd1ec08cc5a6b1d3bf8af44d8e07b
Matheus Antonio C Santos <166297570+matheusantoniocsantos@users.noreply.github.com>
2024-05-16 22:55:46 -0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,14 +1,4 @@
-config = {
-
-    general = {
-        placaItem = '',
-    },
-
-}
-
 -- // troque pela logica do seu inv
-
-
 hasItem = function(player)
     if exports['hash_inventory']:getPlayerItem(player, 'placa', 1) then
         return true
